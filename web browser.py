3e9d93da2df1e18cb0a08648d88110907ce7bb6e
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2025-07-14 10:54:45 +0545

    Update web browser.py

--- web browser.py
+++ web browser.py
@@ -1,3 +1,12 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
 class MainWindow(QMainWindow):
     def __init__(self):
