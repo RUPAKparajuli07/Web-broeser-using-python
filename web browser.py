58285356b070d136d5360c842eaebb975c267fb7
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2025-07-14 10:52:59 +0545

    Update web browser.py

--- web browser.py
+++ web browser.py
@@ -1,8 +1,4 @@
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebEngineWidgets import *
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
